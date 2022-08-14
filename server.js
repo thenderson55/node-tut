16f2e4e190e16bda5fa718822388e3de469ee894
Thomas Henderson <tbhenderson55@yahoo.com>
2022-08-14 14:20:22 +0400

    serve some basic api data

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require("express")
 const path = require("path")
+const { products } = require('./data')
 
 const server = express()
 
-server.use(express.static("./public"))
+// server.use(express.static("./public"))
+
+// server.get("/",(req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./src/index.html"))
+// })
 
 server.get("/",(req, res) => {
-  res.sendFile(path.resolve(__dirname, "./src/index.html"))
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</>' )
+})
+
+server.get("/api/products",(req, res) => {
+  const produstsWithoutDescription = products.map(item => {
+    const {id, name, image} = item
+    return {id,name,image}
+  })
+  res.json(produstsWithoutDescription)
 })
 
 server.get("/about",(req, res) => {
   res.status(200).send("About Page")
 })
 
-server.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource not found</h1>")
-})
+// server.all("*", (req, res) => {
+//   res.status(404).send("<h1>Resource not found</h1>")
+// })
 
 server.listen(4000, () => {
   console.log('Server is listening on port 4000..');
