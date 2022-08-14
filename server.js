e315accd6547f8e81c1f937e11bf351e0c12c17e
Thomas Henderson <tbhenderson55@yahoo.com>
2022-08-14 11:13:38 +0400

    serve index.html

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express")
+const path = require("path")
+
 const server = express()
 
-server.listen(6000, () => {
-  console.log('Server is listening on port 6000..');
+server.get("/",(req, res) => {
+  res.sendFile(path.resolve(__dirname, "./src/index.html"))
+})
+
+server.get("/about",(req, res) => {
+  res.status(200).send("About Page")
+})
+
+server.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found</h1>")
+})
+
+server.listen(4000, () => {
+  console.log('Server is listening on port 4000..');
 })
